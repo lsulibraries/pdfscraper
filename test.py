0a8f4a74d77d2114665dad3ad007ab266f634eea
Jason Peak <jpeak5@lsu.edu>
2016-01-11 11:28:57 -0600

    adding stub test

---
+++ test.py
@@ -0,0 +1,16 @@
+#! python2.7
+
+import unittest
+import pdfScraper
+
+class EadTest(unittest.TestCase):
+
+	def testEad(self):
+		assert True == True, 'bad math'
+
+	def testGetPageNum(self):
+		return
+
+if __name__ == "__main__":
+	unittest.main()
+
