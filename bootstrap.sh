e6843ad124d953342317caad7de6f55029057676
Jason Peak <jpeak5@lsu.edu>
2016-01-13 12:33:34 -0600

    adds zlib to fix missing -lz issue: http://askubuntu.com/questions/377000/compilation-error-usr-bin-ld-cannot-find-lz

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 sudo apt-get update
-sudo apt-get install -y libxml2-dev libxslt-dev python-dev python-pip python-lxml
+sudo apt-get install -y libxml2-dev libxslt-dev python-dev python-pip python-lxml zlib1g-dev
 pip install lxml scraperwiki
