c472611a7312bd018d3419cecff42e4c525b718a
GarrettArm <garrett.w.armstrong@gmail.com>
2016-01-13 12:45:27 -0600

    changed pip to sudo & --upgrade

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get update
 sudo apt-get install -y libxml2-dev libxslt-dev python-dev python-pip python-lxml zlib1g-dev
-pip install lxml scraperwiki
+sudo pip install --upgrade lxml scraperwiki
