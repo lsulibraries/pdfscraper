66caeb4408e1efff5eca72ac0c8c2c1fa0b499cc
GarrettArm <garrett.w.armstrong@gmail.com>
2016-01-13 14:39:05 -0600

    linted test.py & starting to add another test

--- test.py
+++ test.py
@@ -5,10 +5,13 @@ from pdfScraper import FindingAidPDFtoEAD
 import lxml
 
 # http://eli.thegreenplace.net/2011/08/02/python-unit-testing-parametrized-test-cases
+
+
 class ParametrizedTestCase(unittest.TestCase):
     """ TestCase classes that want to be parametrized should
         inherit from this class.
     """
+
     def __init__(self, methodName='runTest', url=None):
         super(ParametrizedTestCase, self).__init__(methodName)
         self.url = url
@@ -26,71 +29,81 @@ class ParametrizedTestCase(unittest.TestCase):
         return suite
 
 
-
-
 class EadTest(ParametrizedTestCase):
 
-	def setUp(self):
-		self.findaid = FindingAidPDFtoEAD(self.url)
+    def setUp(self):
+        self.findaid = FindingAidPDFtoEAD(self.url)
 
-	def test__init__(self):
-		assert len(self.findaid.root) > 0
-		self.assertIsInstance(self.findaid.root, lxml.etree._Element)
+    def test__init__(self):
+        assert len(self.findaid.root) > 0
+        self.assertIsInstance(self.findaid.root, lxml.etree._Element)
 
-	def testGetPageNum(self):
-		terms = ["BIOGRAPHICAL/HISTORICAL NOTE",
-				"SCOPE AND CONTENT NOTE", 
-				"LIST OF SERIES AND SUBSERIES", 
-				"SERIES DESCRIPTIONS", 
-				"INDEX TERMS", 
-				"CONTAINER LIST",
-				#"CITE AS"
-				]
-		self.list_of_proven_answers = 	{'http://www.lib.lsu.edu/sites/default/files/sc/findaid/0826.pdf': {
-										terms[0]: 4, 
-										terms[1]: 4,
-										terms[2]: 18,
-										terms[3]: 18,
-										terms[4]: 5,
-										terms[5]: 6,},
-									'http://www.lib.lsu.edu/sites/default/files/sc/findaid/4452.pdf': {
-										terms[0]: 4,
-										terms[1]: 6,
-										terms[2]: 7,
-										terms[3]: 8, # Fails - uses non-standard header 'SERIES AND SUBSERIES DESCRIPTONS'
-										terms[4]: 15,
-										terms[5]: 18,},
-									'http://www.lib.lsu.edu/sites/default/files/sc/findaid/5078.pdf': {
-										terms[0]: 4,
-										terms[1]: 5,
-										terms[2]: 6,
-										terms[3]: 7,
-										terms[4]: 10,
-										terms[5]: 11,},
-									'http://www.lib.lsu.edu/sites/default/files/sc/findaid/0717.pdf': {
-										terms[0]: 4,
-										terms[1]: 5,
-										terms[2]: 18,
-										terms[3]: 18,
-										terms[4]: 7,
-										terms[5]: 8,},
-									}		
-		allTerms = self.list_of_proven_answers[self.url]
+    def testGetPageNum(self):
+        terms = ["BIOGRAPHICAL/HISTORICAL NOTE",
+                 "SCOPE AND CONTENT NOTE",
+                 "LIST OF SERIES AND SUBSERIES",
+                 "SERIES DESCRIPTIONS",
+                 "INDEX TERMS",
+                 "CONTAINER LIST",
+                 # "CITE AS"
+                 ]
+        self.list_of_proven_answers = {'http://www.lib.lsu.edu/sites/default/files/sc/findaid/0826.pdf': {
+            terms[0]: 4,
+            terms[1]: 4,
+            terms[2]: 18,
+            terms[3]: 18,
+            terms[4]: 5,
+            terms[5]: 6, },
+            'http://www.lib.lsu.edu/sites/default/files/sc/findaid/4452.pdf': {
+            terms[0]: 4,
+            terms[1]: 6,
+            terms[2]: 7,
+            # Fails - uses non-standard header 'SERIES AND SUBSERIES
+            # DESCRIPTONS'
+            terms[3]: 8,
+            terms[4]: 15,
+            terms[5]: 18, },
+            'http://www.lib.lsu.edu/sites/default/files/sc/findaid/5078.pdf': {
+            terms[0]: 4,
+            terms[1]: 5,
+            terms[2]: 6,
+            terms[3]: 7,
+            terms[4]: 10,
+            terms[5]: 11, },
+            'http://www.lib.lsu.edu/sites/default/files/sc/findaid/0717.pdf': {
+            terms[0]: 4,
+            terms[1]: 5,
+            terms[2]: 18,
+            terms[3]: 18,
+            terms[4]: 7,
+            terms[5]: 8, },
+        }
+        allTerms = self.list_of_proven_answers[self.url]
 
-		for term, page_num in allTerms.iteritems():
-			self.assertEquals(self.findaid.getpagenum(term)[0], page_num, '\n\nFor url :{}\nTerm: {}\nExpected Page #: {}\nActual Page #: {}\n'.format(self.url, term, page_num, self.findaid.getpagenum(term)[0]))
+        for term, page_num in allTerms.iteritems():
+            self.assertEquals(self.findaid.getpagenum(term)[0], page_num, '\n\nFor url :{}\nTerm: {}\nExpected Page #: {}\nActual Page #: {}\n'.format(
+                self.url, term, page_num, self.findaid.getpagenum(term)[0]))
 
-	def testgetrcoldata(self):
-		size = self.findaid.getrcoldata('Size.')
+    def testGetRColData(self):
+        # size = self.findaid.getrcoldata('Size.')
+        pass
+
+    def testGetAllText(self):
+        
+        pass
 
 
 if __name__ == "__main__":
-	suite = unittest.TestSuite()
-	suite.addTest(ParametrizedTestCase.parametrize(EadTest, url='http://www.lib.lsu.edu/sites/default/files/sc/findaid/0717.pdf'))
-	suite.addTest(ParametrizedTestCase.parametrize(EadTest, url='http://www.lib.lsu.edu/sites/default/files/sc/findaid/0826.pdf'))
-	suite.addTest(ParametrizedTestCase.parametrize(EadTest, url='http://www.lib.lsu.edu/sites/default/files/sc/findaid/5078.pdf'))
-	suite.addTest(ParametrizedTestCase.parametrize(EadTest, url='http://www.lib.lsu.edu/sites/default/files/sc/findaid/4452.pdf'))
-	unittest.TextTestRunner(verbosity=2).run(suite)
+    suite = unittest.TestSuite()
+    suite.addTest(ParametrizedTestCase.parametrize(
+        EadTest, url='http://www.lib.lsu.edu/sites/default/files/sc/findaid/0717.pdf'))
+    suite.addTest(ParametrizedTestCase.parametrize(
+        EadTest, url='http://www.lib.lsu.edu/sites/default/files/sc/findaid/0826.pdf'))
+    suite.addTest(ParametrizedTestCase.parametrize(
+        EadTest, url='http://www.lib.lsu.edu/sites/default/files/sc/findaid/5078.pdf'))
+    suite.addTest(ParametrizedTestCase.parametrize(
+        EadTest, url='http://www.lib.lsu.edu/sites/default/files/sc/findaid/4452.pdf'))
+    unittest.TextTestRunner(verbosity=2).run(suite)
 
 # styles
 # pre 2010 is really funky
