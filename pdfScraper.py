30b52b6045f6e2fde1237d366b724784f96ee252
GarrettArm <garrett.w.armstrong@gmail.com>
2016-01-18 22:42:45 -0600

    removed all self.variables except self.url and self.element_tree,  others changed to local variables within the def

--- pdfScraper.py
+++ pdfScraper.py
@@ -100,11 +100,10 @@ class FindingAidPDFtoEAD():
         # titleproper - needs to account for multiple lines in some docs
         wholetitle = []
         titlelines = self.element_tree.xpath('//page[@number="1"]/text[@top>="200" and @width>"10"]/b')
-        # print titlelines
 
         for el in titlelines:
             wholetitle.append(el.text.strip())
-        self.pdftitleproper = 'A GUIDE TO THE ' + ' '.join(wholetitle)
+        pdftitleproper = 'A GUIDE TO THE ' + ' '.join(wholetitle)
 
         # figuring out what the top value of the last line of the title is
         titlelineend = titlelines[-1].getparent().get('top')
@@ -117,62 +116,62 @@ class FindingAidPDFtoEAD():
         mss_elem = self.element_tree.xpath(xpath_address)[0]
         mss_text = mss_elem.text
         if mss_text:
-            self.pdfnum = mss_text.strip()
+            pdfnum = mss_text.strip()
         else:
             # should we raise & catch exceptions here?
             print '\nAttention!!   MSS not digitally scanned from document: ', self.url, '\n'
-            self.pdfnum = 'Attention!!   MSS not digitally scanned from document: {}'.format(self.url)
+            pdfnum = 'Attention!!   MSS not digitally scanned from document: {}'.format(self.url)
 
         # author - take next text node after the one that says "Compiled by" - with exception handling
         try:
-            self.pdfauthor = self.element_tree.xpath('//page[@number="1"]/text[text()[normalize-space(.)="Compiled by"]]')[0].getnext().text.strip()
+            pdfauthor = self.element_tree.xpath('//page[@number="1"]/text[text()[normalize-space(.)="Compiled by"]]')[0].getnext().text.strip()
         except:
-            self.pdfauthor = 'Special Collections Staff'
+            pdfauthor = 'Special Collections Staff'
 
         # date - last node over "20" width on first page - "reformatted" or "revised" dates okay?
-        self.pdfdate = self.element_tree.xpath('//page[@number="1"]/text[@width>"20"]')[-1].text.strip()
+        pdfdate = self.element_tree.xpath('//page[@number="1"]/text[@width>"20"]')[-1].text.strip()
 
         # physdesc -
 
         # page 3 has a table - find the left of the two columns - can assume Size is the first and always there?
         elem_for_pos_of_left_column = self.element_tree.xpath('//page[@number="3"]/text/b[text()[normalize-space(.)="Size."]]')
         if elem_for_pos_of_left_column:
-            self.xpos_of_left_column = elem_for_pos_of_left_column[0].getparent().get('left')
+            xpos_of_left_column = elem_for_pos_of_left_column[0].getparent().get('left')
         # should we raise & catch Exceptions?
         else:
             # print '\nAttention!!  elem_for_pos_of_left_column not digitally scanned from document: {}\n'.format(self.url)
-            self.xpos_of_left_column = '/nAttention!!  elem_for_pos_of_left_column not digitally scanned from document: {}'.format(self.url)
+            xpos_of_left_column = '/nAttention!!  elem_for_pos_of_left_column not digitally scanned from document: {}'.format(self.url)
 
         # function finds right hand column data based on text of left hand column - just for page 3
 
-        self.pdfextent = self.getrcoldata("Size.")
-        self.pdfidates = self.getrcoldata("Inclusive dates.")
-        self.pdfbdates = self.getrcoldata("Bulk dates.")
+        pdfextent = self.getrcoldata("Size.")
+        pdfidates = self.getrcoldata("Inclusive dates.")
+        pdfbdates = self.getrcoldata("Bulk dates.")
 
-        self.pdflanguage = self.getrcoldata("Language.")
-        if self.pdflanguage == "":
-            self.pdflanguage = self.getrcoldata("Languages.")
+        pdflanguage = self.getrcoldata("Language.")
+        if pdflanguage == "":
+            pdflanguage = self.getrcoldata("Languages.")
 
-        self.pdfabstract = self.getrcoldata("Summary.")
+        pdfabstract = self.getrcoldata("Summary.")
 
-        self.pdfaccessrestrict = self.getrcoldata("Restrictions on access.")
-        if self.pdfaccessrestrict == "":
-            self.pdfaccessrestrict = self.getrcoldata("Access restrictions.")
+        pdfaccessrestrict = self.getrcoldata("Restrictions on access.")
+        if pdfaccessrestrict == "":
+            pdfaccessrestrict = self.getrcoldata("Access restrictions.")
 
-        self.pdfrelatedmaterial = self.getrcoldata("Related collections.")
-        if self.pdfrelatedmaterial == "":
-            self.pdfrelatedmaterial = self.getrcoldata("Related collection.")
+        pdfrelatedmaterial = self.getrcoldata("Related collections.")
+        if pdfrelatedmaterial == "":
+            pdfrelatedmaterial = self.getrcoldata("Related collection.")
 
-        self.pdfuserestrict = self.getrcoldata("Copyright.")
-        self.pdfprefercite = self.getrcoldata("Citation.")
+        pdfuserestrict = self.getrcoldata("Copyright.")
+        pdfprefercite = self.getrcoldata("Citation.")
 
-        self.pdfphysloc = self.getrcoldata("Stack locations.")
-        if self.pdfphysloc == "":
-            self.pdfphysloc = self.getrcoldata("Stack location.")
+        pdfphysloc = self.getrcoldata("Stack locations.")
+        if pdfphysloc == "":
+            pdfphysloc = self.getrcoldata("Stack location.")
 
         # Then the getalltext function won't need a backupheader, and we can get all text by looping through available terms for appropriate sections
-        self.pdfbioghist = self.getalltext("BIOGRAPHICAL/HISTORICAL NOTE", "SCOPE AND CONTENT NOTE", "LIST OF SERIES AND SUBSERIES")
-        self.pdfscopecontent = self.getalltext("SCOPE AND CONTENT NOTE", "LIST OF SERIES AND SUBSERIES", "INDEX TERMS")
+        pdfbioghist = self.getalltext("BIOGRAPHICAL/HISTORICAL NOTE", "SCOPE AND CONTENT NOTE", "LIST OF SERIES AND SUBSERIES")
+        pdfscopecontent = self.getalltext("SCOPE AND CONTENT NOTE", "LIST OF SERIES AND SUBSERIES", "INDEX TERMS")
         almostListSeries = self.getalltext("LIST OF SERIES AND SUBSERIES", "SERIES DESCRIPTIONS", "INDEX TERMS")
         seriesdesc = self.getalltext("SERIES DESCRIPTIONS", "INDEX TERMS", "CONTAINER LIST")
         finalseries = self.seriesSplit(almostListSeries, "list", "head", "item", False)
@@ -189,65 +188,65 @@ class FindingAidPDFtoEAD():
                     E.eadid('', countrycode='us', url=self.url),
                     E.filedesc(
                         E.titlestmt(
-                            E.titleproper(self.pdftitleproper + '\n\t\t\t',
-                                          E.num(self.pdfnum, type='Manuscript'),
+                            E.titleproper(pdftitleproper + '\n\t\t\t',
+                                          E.num(pdfnum, type='Manuscript'),
                                           ),
                             E.subtitle(pdfsubtitle),
-                            E.author(self.pdfauthor)
+                            E.author(pdfauthor)
                         ),
                         E.publicationstmt(
                             E.publisher(pdfpublisher),
                             E.address(
                                 E.addressline(pdfaddressline),
                             ),
-                            E.date(self.pdfdate)
+                            E.date(pdfdate)
                         )
                     )
                 ),
                 E.archdesc(
                     E.did(
                         E.head(pdfhead),
-                        E.physdesc('' + '\n\t\t', E.extent(self.pdfextent), label='Size', encodinganalog='300$a'),
-                        E.unitdate(self.pdfidates, type='inclusive', label='Dates:', encodinganalog='245$f'),
-                        E.unitdate(self.pdfbdates, type='bulk', label='Dates:'),
+                        E.physdesc('' + '\n\t\t', E.extent(pdfextent), label='Size', encodinganalog='300$a'),
+                        E.unitdate(pdfidates, type='inclusive', label='Dates:', encodinganalog='245$f'),
+                        E.unitdate(pdfbdates, type='bulk', label='Dates:'),
                         E.langmaterial(
-                            E.language(self.pdflanguage, langcode='eng')
+                            E.language(pdflanguage, langcode='eng')
                         ),
-                        E.abstract(self.pdfabstract, label='Summary'),
+                        E.abstract(pdfabstract, label='Summary'),
                         E.repository(
                             E.corpname(pdfcorpname),
                             E.subarea(pdfsubarea),
                             label='Repository:', encodinganalog='825$a'
                         ),
-                        E.physloc(self.pdfphysloc),
+                        E.physloc(pdfphysloc),
                     ),
                     E.accessrestrict(
                         E.head("Restrictions on access"),
-                        E.p(self.pdfaccessrestrict),
+                        E.p(pdfaccessrestrict),
                     ),
                     E.relatedmaterial(
                         E.head("Related Collections"),
-                        E.p(self.pdfrelatedmaterial),
+                        E.p(pdfrelatedmaterial),
                         encodinganalog='544 1'
                     ),
                     E.userestrict(
                         E.head("Copyright"),
-                        E.p(self.pdfuserestrict),
+                        E.p(pdfuserestrict),
                         encodinganalog='540'
                     ),
                     E.prefercite(
                         E.head("Preferred Citation"),
-                        E.p(self.pdfprefercite),
+                        E.p(pdfprefercite),
                         encodinganalog='524'
                     ),
                     E.bioghist(
                         E.head("BIOGRAPHICAL/HISTORICAL NOTE"),
-                        E.p(self.pdfbioghist),
+                        E.p(pdfbioghist),
                         encodinganalog='545'
                     ),
                     E.scopecontent(
                         E.head("SCOPE AND CONTENT NOTE"),
-                        E.p(self.pdfscopecontent),
+                        E.p(pdfscopecontent),
                         encodinganalog='520'
                     ),
                     # INDEX TERMS will need to be encoded all as 'subject' cuz we can't tell automatically...
@@ -275,7 +274,7 @@ class FindingAidPDFtoEAD():
         try:
             rcoltop = str(int(self.element_tree.xpath('//page[@number="3"]/text/b[text()[normalize-space(.)="' + lcolname + '"]]')[0].getparent().get('top')))
             rcoltopbuffer = str(int(rcoltop)-10)
-            afterrcoltop = str(int(self.element_tree.xpath('//page[@number="3"]/text[@left=' + self.xpos_of_left_column + ' and @top=' + rcoltop + ']/following::text[b]')[0].get('top'))-10)
+            afterrcoltop = str(int(self.element_tree.xpath('//page[@number="3"]/text[@left=' + xpos_of_left_column + ' and @top=' + rcoltop + ']/following::text[b]')[0].get('top'))-10)
             # check to see if it's last on page (it loops to top for some reason). if so hopefully one line is needed
             if afterrcoltop < rcoltop:
                 afterrcoltop = str(int(rcoltop)+20)
@@ -289,7 +288,7 @@ class FindingAidPDFtoEAD():
                 rcoltop = str(int(self.element_tree.xpath('//page[@number="3"]/text/b[text()[normalize-space(.)="' + lcolnameshort + '"]]')[0].getparent().get('top')))
                 rcoltopbuffer = str(int(rcoltop)-10)
                 try:  # if it's the last in the column then hopefully its a single line
-                    afterrcoltop = str(int(self.element_tree.xpath('//page[@number="3"]/text[@left=' + self.xpos_of_left_column + ' and @top=' + rcoltop + ']/following::text[b]')[0].get('top'))-10)
+                    afterrcoltop = str(int(self.element_tree.xpath('//page[@number="3"]/text[@left=' + xpos_of_left_column + ' and @top=' + rcoltop + ']/following::text[b]')[0].get('top'))-10)
                 except:
                     afterrcoltop = str(int(rcoltop)+15)
                 datalines = self.element_tree.xpath('//page[@number="3"]/text[@top>' + rcoltopbuffer + 'and @top<' + afterrcoltop + ' and @left>"200"]')
@@ -303,7 +302,7 @@ class FindingAidPDFtoEAD():
                     rcoltop = str(int(self.element_tree.xpath('//page[@number="3"]/text/b[text()[normalize-space(.)="' + lcolnamefirstpart + '"]]')[0].getparent().get('top')))
                     rcoltopbuffer = str(int(rcoltop)-10)
                     nextcoltop = str(int(self.element_tree.xpath('//page[@number="3"]/text/b[text()[normalize-space(.)="' + lcolnamelastword + '"]]')[0].getparent().get('top')))
-                    afterrcoltop = str(int(self.element_tree.xpath('//page[@number="3"]/text[@left=' + self.xpos_of_left_column + ' and @top=' + nextcoltop + ']/following::text[b]')[0].get('top'))-10)
+                    afterrcoltop = str(int(self.element_tree.xpath('//page[@number="3"]/text[@left=' + xpos_of_left_column + ' and @top=' + nextcoltop + ']/following::text[b]')[0].get('top'))-10)
                     datalines = self.element_tree.xpath('//page[@number="3"]/text[@top>' + rcoltopbuffer + 'and @top<' + afterrcoltop + ' and @left>"200"]')
                     for el in datalines:
                         lcoldata.append(el.text.strip())
