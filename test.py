eacab7d9f8f64f5dab12ac6a5adc015d644d353f
flummingbird <wwconlin@gmail.com>
2016-01-19 13:07:07 -0600

    added new test case for 0717

--- test.py
+++ test.py
@@ -39,6 +39,9 @@ class EadTest(ParametrizedTestCase):
         assert len(self.findaid.element_tree) > 0
         self.assertIsInstance(self.findaid.element_tree, lxml.etree._Element)
 
+        #should we have a more unique dictionary of expected terms that are unique to the pdf
+        #it seems like this would fix our problem with the 4452.pdf
+        #or should we just make a new dictionary with the expected terms for each unique pdf?
     def testGetPageNum(self):
         terms = ["BIOGRAPHICAL/HISTORICAL NOTE",
                  "SCOPE AND CONTENT NOTE",
@@ -59,7 +62,7 @@ class EadTest(ParametrizedTestCase):
             terms[0]: 4,
             terms[1]: 6,
             terms[2]: 7,
-            # Fails - uses non-standard header 'SERIES AND SUBSERIES
+            # Fails - uses non-standard header 'SERIES AND SUBSERIES 
             # DESCRIPTONS'
             terms[3]: 8,
             terms[4]: 15,
@@ -143,6 +146,7 @@ class EadTest(ParametrizedTestCase):
         seriesdesc       = self.findaid.seriesSplit(seriesdesc, "co1", "unitid", "p", True)
         # print(finalseries)
 
+        #I feel like this test could be better named... also it's commented out so it always fails
     def testGetDeflistItem(self):
         labels = [
             'Created by',
@@ -175,6 +179,9 @@ class EadTest(ParametrizedTestCase):
             self.assertEquals(observed, value, 'For url: {}\nUnexpected Value for: {}\nGot: {}\nExpected: {}\n\n'.format(
                 self.url, label, observed, value))
 
+
+            #I'm starting to wonder what to do when we encounter a non <p> tag, should we just pass?
+            #question: Do we need to populate expected text for every pdf?
     def testGetTextAfterHeader(self):
         sample_header_and_pages = {'http://www.lib.lsu.edu/sites/default/files/sc/findaid/4452.pdf': (
             ('Biographical/Historical Note', (4, 4),
@@ -192,16 +199,40 @@ class EadTest(ParametrizedTestCase):
             ('B Index to Merchants', (23, 23), """   """),
             ('C List of Music Publishers', (24, 24), """   """),
         ),
+        'http://www.lib.lsu.edu/sites/default/files/sc/findaid/5078.pdf': (
+            ('Summary', (3, 3), """"""),
+            ('Biographical/Historical Note', (4, 4), """Jesse Homer Bankston was born Oct. 7, 1907 to Allie Magee and Leon V. Bankston of Washington Parish, La. He was educated in local schools and received his Ruth Paine (1918&#8211;1997), daughter of Walter R. Paine, Sr. Ruth was a member of the Bankston began his career in government in 1940 under Governor As a management consultant, he was charged with the reorganization of state government. In 1942, he became an organizational specialist in the Louisiana Civil Service Department. Under Governor assistant in the Department of Institutions and later as the director of that same Department. The Department, which was created 1940 and re-organized in 1942, supervised all mental, tuberculosis, and general hospitals, together with the State Penitentiary and two institutions for delinquent youths. When Governor Earl K. Long left office in 1952, Bankston left state government to open a healthcare consulting firm. With Governor Long&#8217;s return to office in 1956, Bankston was appointed the director of the newly established Department of Hospitals. He held this position until the summer of 1959, when he was dismissed after a dispute over the governor&#8217;s mental health. After his dismissal, Bankston returned to his consulting firm, and began to work with Democratic Party candidates and issues. He also joined the boards of the newly established Louisiana Public Broadcasting, and State Board of Elementary and Secondary Education. Elected in 1968, he served on State Board of Elementary and Secondary Education for 28 years. During his tenure, he served as secretary and chairman of the Board. Bankston was also the longest serving member of the Louisiana Democratic State Central Committee, having served for 51 years until his death in 2010. In 2002, he was inducted into the Louisiana Political Hall of Fame. In addition to his political pursuits, he also served as president of the Baton Rouge YMCA, Young Men's Business Club, Mental Health Association, and Tuberculosis Society."""),
+            ('Scope and Content Note', (5, 5), """The collection, consisting of correspondence, political files, printed items and photographs, reflects Jesse H. Bankston&#8217;s involvement in Louisiana state government and the Louisiana Democratic Party. Correspondence discusses political elections, candidates, and government health services. Political files pertain to activities of the Democratic Party, Louisiana politicians, and Louisiana Department of Institutions and State Hospital Board. Printed items contain ephemera and published material. Ephemera include programs, invitations, campaign buttons and bumper stickers, and items relating to the Mardi Gras celebration in Washington, D. C. Published material includes books, serials, newspaper clippings, and scrapbooks relating to Louisiana politics, campaigns and the National Democratic Convention of 1976. Photographs contain portraits, group photographs, and snapshots, with group photographs making up a large part of this series. Early photographs show several Bankston family members (ca. 1930s-ca. 1957). The remaining images show politicians and members of the Democratic Party at political events. The collection also includes a few miscellaneous personal papers."""),
+            ('List of Series and Subseries', (6, 6), """   """),
+            ('Series Descriptons', (7, 7), """   """),
+            ('Index Terms', (10, 10), """   """),
+            ('Container List', (18, 18), """   """),
+        ),
         }
         current_pdfs_expected_answer = sample_header_and_pages[self.url]
         for section, page_nums, expected_text in current_pdfs_expected_answer:
             observed_result = self.findaid.get_text_after_header(
                 section, page_nums)
         self.assertEquals(observed_result, expected_text, 'For url: {}\nUnexpected Value for: {}\nGot: {}\nExpected: {}\n\n'.format(
+        
             self.url, section, observed_result, expected_text))
 
 
     def testCollapser(self):
+            otherunit = '''<unit><text top="174" left="135" width="647" height="16" font="0"><a href="tmpqOHeUC.html#3">SUMMARY ........................................................................................................................ 3</a></text>
+                <text top="170" left="782" width="4" height="21" font="2"> </text>
+                <text top="195" left="135" width="647" height="16" font="0"><a href="tmpqOHeUC.html#4">BIOGRAPHICAL/HISTORICAL NOTE .......................................................................... 4</a></text>
+                <text top="191" left="782" width="4" height="21" font="2"> </text>
+                <text top="215" left="135" width="647" height="16" font="0"><a href="tmpqOHeUC.html#5">SCOPE AND CONTENT NOTE ....................................................................................... 5</a></text>
+                <text top="212" left="782" width="4" height="21" font="2"> </text>
+                <text top="236" left="135" width="647" height="16" font="0"><a href="tmpqOHeUC.html#6">LIST OF SERIES AND SUBSERIES ................................................................................ 6</a></text>
+                <text top="232" left="782" width="4" height="21" font="2"> </text>
+                <text top="257" left="135" width="647" height="16" font="0"><a href="tmpqOHeUC.html#7">SERIES DESCRIPTIONS .................................................................................................. 7</a></text>
+                <text top="253" left="782" width="4" height="21" font="2"> </text>
+                <text top="278" left="135" width="647" height="16" font="0"><a href="tmpqOHeUC.html#10">INDEX TERMS ................................................................................................................ 10</a></text>
+                <text top="274" left="782" width="4" height="21" font="2"> </text>
+                <text top="298" left="135" width="647" height="16" font="0"><a href="tmpqOHeUC.html#11">CONTAINER LIST .......................................................................................................... 11</a></text>
+                </unit>'''
         unit = '''<unit>
                 <text top="112" left="108" width="251" height="16" font="0"><b>ACY (WILLIAM) JR. PAPERS </b></text>
                 <text top="106" left="360" width="5" height="24" font="1"> </text>
@@ -258,7 +289,7 @@ class EadTest(ParametrizedTestCase):
         path = '//text[b[contains(text(), "CONTENTS OF INVENTORY")]]/following-sibling::text/a'
         elms = tree.xpath(path)
         collapsed = self.findaid.collapse(elms)
-        # print collapsed
+        print collapsed
 
     def testCollapseRealPdfs(self):
         contents = self.findaid.element_tree.xpath('//page/text[b[contains(text(), "CONTENTS OF INVENTORY")]]/following-sibling::text/a')
@@ -267,14 +298,14 @@ class EadTest(ParametrizedTestCase):
 if __name__ == "__main__":
     #  dev -- don't worry about tests calling out on the internet -- pdfScraper.read_url_return_etree() is switched to read from cached file.
     suite = unittest.TestSuite()
-    # suite.addTest(ParametrizedTestCase.parametrize(
-    #     EadTest, url='http://www.lib.lsu.edu/sites/default/files/sc/findaid/0717.pdf'))
-    # suite.addTest(ParametrizedTestCase.parametrize(
-    #     EadTest, url='http://www.lib.lsu.edu/sites/default/files/sc/findaid/0826.pdf'))
-    # suite.addTest(ParametrizedTestCase.parametrize(
-    #     EadTest, url='http://www.lib.lsu.edu/sites/default/files/sc/findaid/5078.pdf'))
     suite.addTest(ParametrizedTestCase.parametrize(
-        EadTest, url='http://www.lib.lsu.edu/sites/default/files/sc/findaid/4452.pdf'))
+       EadTest, url='http://www.lib.lsu.edu/sites/default/files/sc/findaid/0717.pdf'))
+    # suite.addTest(ParametrizedTestCase.parametrize(
+    #    EadTest, url='http://www.lib.lsu.edu/sites/default/files/sc/findaid/0826.pdf'))
+    suite.addTest(ParametrizedTestCase.parametrize(
+        EadTest, url='http://www.lib.lsu.edu/sites/default/files/sc/findaid/5078.pdf'))
+    # suite.addTest(ParametrizedTestCase.parametrize(
+    #    EadTest, url='http://www.lib.lsu.edu/sites/default/files/sc/findaid/4452.pdf'))
     unittest.TextTestRunner(verbosity=2).run(suite)
 
 # styles
