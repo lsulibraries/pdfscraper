a2522aa05cff034d1a42d943f6e6a30b9d8e26d3
GarrettArm <garrett.w.armstrong@gmail.com>
2016-01-25 16:10:05 -0600

    added source info to subject terms dict

--- one_off_scripts/txt_to_dict.py
+++ one_off_scripts/txt_to_dict.py
@@ -2,26 +2,41 @@
 # small script to convert text to dict file
 
 subject_term_sets = {
-                     'geoname': set(),
-                     'persname': set(),
-                     'subject': set(),
-                     'title_subject': set(),
-                     'occupation': set(),
-                     'genreform': set(),
-                     'corpname': set(),
-                     }
+                     'geoname':     {'lcsh': set(),},
+                     'persname':    {'lcsh': set(),},
+                     'subject':     {'aat':  set(),
+                                    'lcsh':  set(),
+                                    'local': set(),
+                                    },
+                     'title_subject': {'lcsh': set(),},
+                     'occupation':  {'lcsh': set(),
+                                    'local': set(),
+                                    },
+                     'genreform':   {'aat': set(),
+                                    'gmgpc': set(),
+                                    'lcsh': set(),
+                                    'local': set(),
+                                    },
+                     'corpname':    {'local': set(),
+                                    'lcsh': set(),
+                                    },
+                    }
 
-for term, term_set in subject_term_sets.iteritems():
-    with open('field_text_files/{}.txt'.format(term)) as f:
-        for line in f:
-            line = line.strip('\n\r').strip()
-            term_set.add(line)
+for term, source_dict in subject_term_sets.iteritems():
+    for source, term_set in source_dict.iteritems():
+        with open('field_text_files/{}-{}.txt'.format(term, source)) as f:
+            for line in f:
+                line = line.strip('\n\r').strip()
+                term_set.add(line)
 
 # show items in two sets
-# for term, term_set in subject_term_sets.iteritems():
-#     for uterm, uterm_set in subject_term_sets.iteritems():
-#         if term != uterm:
-#             new_union = term_set.intersection(uterm_set)
-#             print term, '\t', uterm, ": ", new_union
+dupe_set = []
+for term, source_dict in subject_term_sets.iteritems():
+    for source, term_set in source_dict.iteritems():
+        for uterm, usource_dict in subject_term_sets.iteritems():
+            for usource, uterm_set in usource_dict.iteritems():
+                if (term, source, term_set) != (uterm, usource, uterm_set):
+                    new_union = term_set.intersection(uterm_set)
+                    print term, source, '\t', uterm, usource,": ", new_union
 
-print 'terms_sets_dict = {}'.format(subject_term_sets)
+#print 'terms_sets_dict = {}'.format(subject_term_sets)
